139f20e3c8893cbf86e3711bc5a6068adbd23804
mohnurulanwar <mohnurulanwar89@gmail.com>
2023-05-24 00:26:50 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+  {
+    id: "abc",
+    title: "Catatan A",
+    tags: ["nodejs", "backend"],
+    body: "Ini adalah catatan A",
+    createdAt: "2021-03-02T09:00:00.000Z",
+    updatedAt: "2021-03-02T09:00:00.000Z",
+  },
+];
+
+module.exports = notes;
